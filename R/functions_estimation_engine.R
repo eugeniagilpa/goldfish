b00441a05ff4bd49acb28aa8ac26e19f86d36ef4
María Eugenia Gil-Pallares <mariaeugeniagilpallares@gmail.com>
2024-06-07 11:54:51 +0200

    new change

--- R/functions_estimation_engine.R
+++ R/functions_estimation_engine.R
@@ -314,18 +314,18 @@ estimate_int <- function(
     # It's for the fixing parameter feature. \
     score[idFixedCompnents] <- 0
 
-    # if (onlyScore){
-    #   isConverged <- TRUE
-    #   break
-    # }
-    if (onlyScore) {
-      return(list(
-        logLikelihood = logLikelihood,
-        finalScore = score,
-        finalInformationMatrix = informationMatrix,
-        nEvents = nEvents
-      ))
+    if (onlyScore){
+      isConverged <- TRUE
+      break
     }
+    # if (onlyScore) {
+    #   return(list(
+    #     logLikelihood = logLikelihood,
+    #     finalScore = score,
+    #     finalInformationMatrix = informationMatrix,
+    #     nEvents = nEvents
+    #   ))
+    # }
 
     if (!verbose && progress) {
       cat(
